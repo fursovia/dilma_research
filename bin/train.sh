c8bc15a7e427b664158c331136086229e13b0067
fursovia <fursov.ia@gmail.com>
2020-11-19 00:34:52 +0300

    fixed

--- bin/train.sh
+++ bin/train.sh
@@ -17,4 +17,4 @@ LOG_DIR=./logs/${EXP_NAME}
 TRAIN_DATA_PATH=${TRAIN_PATH} \
     VALID_DATA_PATH=${VALID_PATH} \
     allennlp train ${CONFIG_PATH} \
-    --serialization-dir ${LOG_DIR}#!/usr/bin/env bash
+    --serialization-dir ${LOG_DIR}
