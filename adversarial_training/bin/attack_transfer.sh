6a78ea73da8cf0485ed75bffee76adf17cf53413
Pavel <pavel.burnysheff@yandex.ru>
2020-12-30 04:16:30 +0500

    fix

--- adversarial_training/bin/attack_transfer.sh
+++ adversarial_training/bin/attack_transfer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 num_examples=1000
-attack_num_examples=1000
+attack_num_examples=500
 batch_size=64
 logging_steps=75
 
@@ -11,23 +11,27 @@ logger_name=$2
 echo "================================" >> ${logger_name}.txt
 echo "dataset ${dataset}" >> ${logger_name}.txt
 
-if [ $dataset == "tomato" ]; then
-split_name_validation='validation'
-split_name_test='test'
+if [ $dataset == "news" ]; then
+validation_split_name='test'
 else
-split_name_validation='validation'
-split_name_test='test'
+validation_split_name='validation'
+fi
+
+if [ $dataset == "sst2" ]; then
+test_split_name='validation'
+else
+test_split_name='test'
 fi
 
 echo "split_name_validation ${split_name_validation}" >> ${logger_name}.txt
-echo "split_name_test ${split_name_test}" >> ${logger_name}.txt
+echo "test_split_name ${test_split_name}" >> ${logger_name}.txt
 
 
-for attacker_under in "deepwordbug" "textbugger" "pwws"; do
+for attacker in "deepwordbug" "textbugger" "pwws"; do
     echo "Start Attack from ${attacker_under} on initial model" >> ${logger_name}.txt
     textattack attack \
-        --dataset-from-file datasets/${dataset}/data/load_${dataset}_${split_name_test}.py \
-        --log-to-csv datasets/${dataset}/attacks/${attacker_from}_0.csv \
+        --dataset-from-file datasets/${dataset}/data/load_${dataset}_${test_split_name}.py \
+        --log-to-csv datasets/${dataset}/attacks/${attacker}_0.csv \
         --recipe ${attacker} \
         --num-examples ${attack_num_examples} \
         --model-from-file datasets/${dataset}/model/load_${dataset}_model.py \
@@ -39,6 +43,7 @@ for num_examples in 100 200 500 1000 1500 2000 3000 5000 10000; do
     echo "================================" >> ${logger_name}.txt
     for attacker_from in "deepwordbug" "textbugger" "pwws"; do
         echo "Additional Train ${attacker_from} ${num_examples} examples start" >> ${logger_name}.txt
+        
         python src/train_model.py \
             --model_name_or_path datasets/${dataset}/model/ \
             --config_name datasets/${dataset}/model/ \
@@ -50,25 +55,26 @@ for num_examples in 100 200 500 1000 1500 2000 3000 5000 10000; do
             --per_device_train_batch_size ${batch_size} \
             --per_device_eval_batch_size ${batch_size} \
             --learning_rate 1e-5 \
-            --num_train_epochs 10.0 \
+            --num_train_epochs 10 \
             --save_steps -1 \
             --evaluation_strategy "epoch" \
             --evaluate_during_training \
+            --output_dir datasets/${dataset}/adv_trained_model/ \
             --adversarial_data_path datasets/${dataset}/attacks/${attacker_from}_${validation_split_name}_max_number.csv \
-            --validation_split_name ${split_name_validation} \
             --adversarial_training_original_data_amount ${num_examples} \
-            --adversarial_training_perturbed_data_amount ${num_examples} \
-            --output_dir datasets/${dataset}/adv_trained_model/
+            --adversarial_training_perturbed_data_amount ${num_examples} 
+            --use_custom_trainer \
+            --stat_file_for_saving stat_${attacker}_${num_examples}.json
 
-        echo "Additional Train ${attacker_from} end" >> ${logger_name}.txt
+        echo "Additional Train ${attacker_from} ${num_examples} examples end" >> ${logger_name}.txt
 
         for attacker_under in "deepwordbug" "textbugger" "pwws"; do
             echo "Begin attack from ${attacker_from} to ${attacker_under} start" >> ${logger_name}.txt
 
             textattack attack \
-                --dataset-from-file datasets/${dataset}/data/load_${dataset}_${split_name_test}.py \
+                --dataset-from-file datasets/${dataset}/data/load_${dataset}_${test_split_name}.py \
                 --log-to-csv datasets/${dataset}/attacks/from_${attacker_from}_to_${attacker_under}_${num_examples}.csv \
-                --recipe ${attacker} \
+                --recipe ${attacker_under} \
                 --num-examples ${attack_num_examples} \
                 --model-from-file datasets/${dataset}/adv_trained_model/load_${dataset}_model.py \
                 --disable-stdout
--- adversarial_training/src/train_model.py
+++ adversarial_training/src/train_model.py
@@ -8,16 +8,17 @@ from torch.utils.data import DataLoader
 from utils import random_seed, get_data, CustomDataset, compute_metrics, get_adv_data
 from dataclasses import dataclass, field
 from typing import Optional
-from transformers import (AutoTokenizer, 
-                          AutoConfig, 
-                          AutoModelForSequenceClassification, 
-                          Trainer, 
+from transformers import (AutoTokenizer,
+                          AutoConfig,
+                          AutoModelForSequenceClassification,
+                          Trainer,
                           HfArgumentParser,
                           TrainingArguments,
                           get_linear_schedule_with_warmup
-                         )
+                          )
 from transformers import default_data_collator
 from custom_trainer import CustomTrainer
+
 
 @dataclass
 class DataTrainingArguments:
@@ -42,6 +43,7 @@ class DataTrainingArguments:
         default=True,
     )
 
+
 @dataclass
 class ModelArguments:
     """
@@ -49,17 +51,21 @@ class ModelArguments:
     """
 
     model_name_or_path: str = field(
-        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}, 
+        metadata={
+            "help": "Path to pretrained model or model identifier from huggingface.co/models"},
         default='roberta-base'
     )
     config_name: Optional[str] = field(
-        metadata={"help": "Pretrained config name or path if not the same as model_name"},
+        metadata={
+            "help": "Pretrained config name or path if not the same as model_name"},
         default='roberta-base'
     )
     tokenizer_name: Optional[str] = field(
-        metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"},
+        metadata={
+            "help": "Pretrained tokenizer name or path if not the same as model_name"},
         default='roberta-base'
     )
+
 
 @dataclass
 class OtherArguments:
@@ -67,97 +73,115 @@ class OtherArguments:
     Some other Arguments
     """
     device: str = field(
-        metadata={"help": "Train using Cuda or CPU"}, 
+        metadata={"help": "Train using Cuda or CPU"},
         default='cuda',
     )
     use_scheduler: str = field(
-        metadata={"help": "Use linear scheduler or not"}, 
+        metadata={"help": "Use linear scheduler or not"},
         default=None,
     )
     adversarial_data_path: str = field(
-        metadata={"help": "path to adv data"}, 
+        metadata={"help": "path to adv data"},
         default=None,
     )
     adversarial_training_original_data_amount: int = field(
-        metadata={"help": "number of non-adversarial sentences to use during adv. training"}, 
+        metadata={
+            "help": "number of non-adversarial sentences to use during adv. training"},
         default=1000,
     )
     adversarial_training_perturbed_data_amount: int = field(
-        metadata={"help": "number of adversarial sentences to use during adv. training"}, 
+        metadata={
+            "help": "number of adversarial sentences to use during adv. training"},
         default=1000,
     )
     validation_split_name: str = field(
-        metadata={"help": "use validation or test split for testing model"}, 
+        metadata={"help": "use validation or test split for testing model"},
         default=None,
     )
     use_custom_trainer: bool = field(
-        metadata={"help": "use custom Trainer or not"}, 
+        metadata={"help": "use custom Trainer or not"},
         default=False,
     )
     stat_file_for_saving: str = field(
-        metadata={"help": "name for statistic of training saving"}, 
+        metadata={"help": "name for statistic of training saving"},
         default=None,
     )
-    
+
 
 def main():
-    
-    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments, OtherArguments))
+
+    parser = HfArgumentParser(
+        (ModelArguments,
+         DataTrainingArguments,
+         TrainingArguments,
+         OtherArguments))
     model_args, data_args, training_args, other_args = parser.parse_args_into_dataclasses()
     training_args.disable_tqdm = True
-    
-    assert data_args.task_name in ['tomato', 'sst2', 'news', 'dstc'], 'Unknown dataset'
-    
+
+    assert data_args.task_name in [
+        'tomato', 'sst2', 'news', 'dstc'], 'Unknown dataset'
+
     device = torch.device(other_args.device)
     if training_args.seed is not None:
         random_seed(training_args.seed)
-    tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name) 
-    
+    tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name)
+
     if other_args.adversarial_data_path is None:
         train_data = get_data(f"datasets/{data_args.task_name}/data/train.txt")
+        num_labels = len(sorted(set([i[1] for i in train_data])))
     else:
         train_data = get_data(f"datasets/{data_args.task_name}/data/train.txt")
-        train_data = random.sample(train_data, min(len(train_data), other_args.adversarial_training_original_data_amount))
+        num_labels = len(sorted(set([i[1] for i in train_data])))
+        train_data = random.sample(
+            train_data, min(
+                len(train_data), other_args.adversarial_training_original_data_amount))
         adv_data = get_adv_data(other_args.adversarial_data_path)
-        adv_data = random.sample(adv_data, min(len(adv_data), other_args.adversarial_training_perturbed_data_amount))
+        adv_data = random.sample(
+            adv_data, min(
+                len(adv_data), other_args.adversarial_training_perturbed_data_amount))
         train_data = train_data + adv_data
         random.sample(train_data, len(train_data))
-        
-        
+
     random.sample(train_data, len(train_data))
-    train_encodings = tokenizer([i[0] for i in train_data], 
-                                truncation=True, 
-                                padding='max_length', 
-                                max_length = data_args.max_seq_length)
+    train_encodings = tokenizer([i[0] for i in train_data],
+                                truncation=True,
+                                padding='max_length',
+                                max_length=data_args.max_seq_length)
     train_dataset = CustomDataset(train_encodings, [i[1] for i in train_data])
-    
+
     if other_args.validation_split_name is None:
         other_args.validation_split_name = 'validation' if data_args.task_name != 'news' else 'test'
-    test_data = get_data(f"datasets/{data_args.task_name}/data/{other_args.validation_split_name}.txt")
-    test_encodings = tokenizer([i[0] for i in test_data], 
-                                truncation=True, 
-                                padding='max_length', 
-                                max_length = data_args.max_seq_length)
+    test_data = get_data(
+        f"datasets/{data_args.task_name}/data/{other_args.validation_split_name}.txt")
+    random.sample(test_data, min(1000, len(test_data)))
+    test_encodings = tokenizer([i[0] for i in test_data],
+                               truncation=True,
+                               padding='max_length',
+                               max_length=data_args.max_seq_length)
     test_dataset = CustomDataset(test_encodings, [i[1] for i in test_data])
 
-    config = AutoConfig.from_pretrained(model_args.config_name, num_labels = len(sorted(set(train_dataset.labels))))
-    model = AutoModelForSequenceClassification.from_pretrained(model_args.model_name_or_path, config = config)
-    
+    config = AutoConfig.from_pretrained(
+        model_args.config_name, num_labels=num_labels)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_args.model_name_or_path, config=config)
 
-    batches_in_epoch = len(train_dataset) // training_args.per_device_train_batch_size + int(len(train_dataset)% training_args.per_device_train_batch_size > 0) 
+    batches_in_epoch = len(train_dataset) // training_args.per_device_train_batch_size + \
+        int(len(train_dataset) % training_args.per_device_train_batch_size > 0)
     training_args.max_steps = training_args.num_train_epochs * batches_in_epoch
     training_args.warmup_steps = batches_in_epoch
     training_args.eval_steps = batches_in_epoch
-    
-    optimizer = torch.optim.AdamW(model.parameters(), lr=training_args.learning_rate)
-    
+
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=training_args.learning_rate)
+
     if other_args.use_scheduler is not None:
-        scheduler = get_linear_schedule_with_warmup(optimizer, 
+        scheduler = get_linear_schedule_with_warmup(optimizer,
                                                     num_warmup_steps=training_args.warmup_steps,
                                                     num_training_steps=training_args.max_steps)
     else:
         scheduler = None
-    
+
     trainer_class = CustomTrainer if other_args.use_custom_trainer else Trainer
     trainer = trainer_class(
         model=model,
@@ -166,26 +190,35 @@ def main():
         eval_dataset=test_dataset,
         compute_metrics=compute_metrics,
         tokenizer=tokenizer,
-        optimizers = (optimizer, scheduler),
-        data_collator = default_data_collator,
+        optimizers=(optimizer, scheduler),
+        data_collator=default_data_collator,
     )
-    
+
     if training_args.do_train:
-        trainer.train(
-            model_path=model_args.model_name_or_path if os.path.isdir(model_args.model_name_or_path) else None,
-            stat_file_for_saving = other_args.stat_file_for_saving
-        )
-    
+        if other_args.use_custom_trainer:
+            trainer.train(
+                model_path=model_args.model_name_or_path if os.path.isdir(
+                    model_args.model_name_or_path) else None,
+                stat_file_for_saving=other_args.stat_file_for_saving
+            )
+        else:
+            trainer.train(
+                model_path=model_args.model_name_or_path if os.path.isdir(
+                    model_args.model_name_or_path) else None,
+            )
+
         trainer.save_model()
-    
+
     if training_args.do_eval:
         eval_result = trainer.evaluate(eval_dataset=test_dataset)
-        output_eval_file = os.path.join(training_args.output_dir, f"eval_results_{data_args.task_name}.txt")
+        output_eval_file = os.path.join(
+            training_args.output_dir,
+            f"eval_results_{data_args.task_name}.txt")
         if trainer.is_world_process_zero():
             with open(output_eval_file, "w") as writer:
                 for key, value in eval_result.items():
                     writer.write(f"{key} = {value}\n")
 
+
 if __name__ == '__main__':
     main()
-
