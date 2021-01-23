7f59681b200f4d0155f40321a3b5d84c505fac2e
fursovia <fursov.ia@gmail.com>
2021-01-23 23:04:14 +0300

    scripts for DL

--- bin/02_prepare_deep_lev_dataset.sh
+++ bin/02_prepare_deep_lev_dataset.sh
@@ -3,3 +3,6 @@
 # 1. take all datasets for text classification
 # 2. sample from bert with different temperatures and masking
 # 3. save the dataset to ./data/deep_levenshtein
+
+
+PYTHONPATH=. python scripts/create_levenshtein_dataset.py
--- bin/03_train_deep_levenshtein.sh
+++ bin/03_train_deep_levenshtein.sh
@@ -5,3 +5,17 @@
 
 
 # allennlp train configs/models/deep_levenshtein.jsonnet
+
+CONFIG_PATH=./configs/models/deep_lev.jsonnet
+DATE=$(date +%H%M%S-%d%m)
+EXP_NAME=${DATE}-deep_levenshtein
+LOG_DIR=./logs/${EXP_NAME}
+
+TRAIN_DATA_PATH=./data/deeplev/train.json \
+  VALID_DATA_PATH=./data/deeplev/valid.json \
+  allennlp train ${CONFIG_PATH} \
+  --serialization-dir ${LOG_DIR} \
+  --include-package dilma
+
+
+cp ${LOG_DIR}/model.tar.gz ./presets/deeplev.tar.gz
