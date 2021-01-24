82d115af0eb8d28bc415cb1fa90cc8e127449bf0
Pavel <pavel.burnysheff@yandex.ru>
2021-01-24 16:55:48 +0300

    train target classifier

---
+++ scripts/custom_trainer.py
@@ -0,0 +1,381 @@
+"""
+The same one as in Huggingface 3.3.0.
+But saves model only if validation accuracy (after epoch end) is the best one.
+Stops, if validation loss raises for several epochs.
+"""
+from packaging import version
+import numpy as np
+import os
+import warnings
+
+
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+from pathlib import Path
+import json
+from tqdm import trange, tqdm
+
+import torch
+from torch import nn
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data.dataset import Dataset
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data.sampler import RandomSampler, SequentialSampler
+
+from transformers import Trainer
+from transformers.file_utils import is_torch_tpu_available
+from transformers.trainer_utils import EvaluationStrategy, TrainOutput
+from transformers.utils import logging
+logger = logging.get_logger(__name__)
+
+
+# Check if Pytorch version >= 1.6 to switch between Native AMP and Apex
+if version.parse(torch.__version__) < version.parse("1.6"):
+    from .file_utils import is_apex_available
+
+    if is_apex_available():
+        from apex import amp
+else:
+    _is_native_amp_available = True
+    from torch.cuda.amp import autocast
+
+
+class CustomTrainer(Trainer):
+    def train(self, model_path: Optional[str] = None, 
+              trial: Union["optuna.Trial",Dict[str, Any]] = None, 
+              stat_file_for_saving: Optional[str] = None,
+              early_stopping_epochs: Optional[int] = 2
+             ):
+        """
+        Main training entry point.
+
+        Args:
+            model_path (:obj:`str`, `optional`):
+                Local path to the model if the model to train has been instantiated from a local path. If present,
+                training will resume from the optimizer/scheduler states loaded here.
+            trial (:obj:`optuna.Trial` or :obj:`Dict[str, Any]`, `optional`):
+                The trial run or the hyperparameter dictionary for hyperparameter search.
+        """
+        # This might change the seed so needs to run first.
+        self._hp_search_setup(trial)
+
+        # Model re-init
+        if self.model_init is not None:
+            # Seed must be set before instantiating the model when using
+            # model_init.
+            set_seed(self.args.seed)
+            model = self.model_init()
+            self.model = model.to(self.args.device)
+
+            # Reinitializes optimizer and scheduler
+            self.optimizer, self.lr_scheduler = None, None
+
+        # Data loader and number of training steps
+        train_dataloader = self.get_train_dataloader()
+        num_update_steps_per_epoch = len(
+            train_dataloader) // self.args.gradient_accumulation_steps
+        num_update_steps_per_epoch = max(num_update_steps_per_epoch, 1)
+        if self.args.max_steps > 0:
+            t_total = self.args.max_steps
+            num_train_epochs = self.args.max_steps // num_update_steps_per_epoch + int(
+                self.args.max_steps % num_update_steps_per_epoch > 0
+            )
+        else:
+            t_total = int(
+                num_update_steps_per_epoch *
+                self.args.num_train_epochs)
+            num_train_epochs = self.args.num_train_epochs
+            self.args.max_steps = t_total
+
+        self.create_optimizer_and_scheduler(num_training_steps=t_total)
+
+        # Check if saved optimizer or scheduler states exist
+        if (
+            model_path is not None
+            and os.path.isfile(os.path.join(model_path, "optimizer.pt"))
+            and os.path.isfile(os.path.join(model_path, "scheduler.pt"))
+        ):
+            # Load in optimizer and scheduler states
+            self.optimizer.load_state_dict(
+                torch.load(
+                    os.path.join(
+                        model_path,
+                        "optimizer.pt"),
+                    map_location=self.args.device)
+            )
+            with warnings.catch_warnings(record=True) as caught_warnings:
+                self.lr_scheduler.load_state_dict(torch.load(
+                    os.path.join(model_path, "scheduler.pt")))
+            reissue_pt_warnings(caught_warnings)
+
+        model = self.model
+        if self.args.fp16 and _use_apex:
+            if not is_apex_available():
+                raise ImportError(
+                    "Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
+            model, self.optimizer = amp.initialize(
+                model, self.optimizer, opt_level=self.args.fp16_opt_level)
+
+        # multi-gpu training (should be after apex fp16 initialization)
+        if self.args.n_gpu > 1:
+            model = torch.nn.DataParallel(model)
+
+        # Distributed training (should be after apex fp16 initialization)
+        if self.args.local_rank != -1:
+            model = torch.nn.parallel.DistributedDataParallel(
+                model,
+                device_ids=[self.args.local_rank],
+                output_device=self.args.local_rank,
+                find_unused_parameters=True,
+            )
+
+        # Train!
+        if is_torch_tpu_available():
+            total_train_batch_size = self.args.train_batch_size * xm.xrt_world_size()
+        else:
+            total_train_batch_size = (
+                self.args.train_batch_size
+                * self.args.gradient_accumulation_steps
+                * (torch.distributed.get_world_size() if self.args.local_rank != -1 else 1)
+            )
+        logger.info("***** Running training *****")
+        logger.info("  Num examples = %d", self.num_examples(train_dataloader))
+        logger.info("  Num Epochs = %d", num_train_epochs)
+        logger.info(
+            "  Instantaneous batch size per device = %d",
+            self.args.per_device_train_batch_size)
+        logger.info(
+            "  Total train batch size (w. parallel, distributed & accumulation) = %d",
+            total_train_batch_size)
+        logger.info("  Gradient Accumulation steps = %d",
+                    self.args.gradient_accumulation_steps)
+        logger.info("  Total optimization steps = %d", t_total)
+
+        self.global_step = 0
+        self.epoch = 0
+        self.total_flos = 0
+        epochs_trained = 0
+        steps_trained_in_current_epoch = 0
+        # Check if continuing training from a checkpoint
+        if model_path is not None:
+            # set global_step to global_step of last saved checkpoint from
+            # model path
+            try:
+                self.global_step = int(model_path.split(
+                    "-")[-1].split(os.path.sep)[0])
+                self.total_flos = getattr(
+                    self._actual_model(model).config, "total_flos", 0)
+
+                epochs_trained = self.global_step // num_update_steps_per_epoch
+                steps_trained_in_current_epoch = self.global_step % (
+                    num_update_steps_per_epoch)
+
+                logger.info(
+                    "  Continuing training from checkpoint, will skip to saved global_step")
+                logger.info(
+                    "  Continuing training from epoch %d",
+                    epochs_trained)
+                logger.info(
+                    "  Continuing training from global step %d",
+                    self.global_step)
+                logger.info(
+                    "  Continuing training from %d non-embedding floating-point operations",
+                    self.total_flos)
+                logger.info(
+                    "  Will skip the first %d steps in the first epoch",
+                    steps_trained_in_current_epoch)
+            except ValueError:
+                self.global_step = 0
+                self.total_flos = 0
+                logger.info("  Starting fine-tuning.")
+
+        validation_stat = {
+            'accuracy': np.zeros(int(num_train_epochs)),
+            'loss': np.zeros(int(num_train_epochs))
+        }
+
+        tr_loss = torch.tensor(0.0).to(self.args.device)
+        logging_loss_scalar = 0.0
+        model.zero_grad()
+        disable_tqdm = self.args.disable_tqdm or not self.is_local_process_zero()
+        train_pbar = trange(epochs_trained,
+                            int(np.ceil(num_train_epochs)),
+                            desc="Epoch",
+                            disable=disable_tqdm)
+        
+        for epoch in range(epochs_trained, int(np.ceil(num_train_epochs))):
+
+            if isinstance(train_dataloader, DataLoader) and isinstance(
+                    train_dataloader.sampler, DistributedSampler):
+                train_dataloader.sampler.set_epoch(epoch)
+            if is_torch_tpu_available():
+                parallel_loader = pl.ParallelLoader(train_dataloader, [self.args.device]).per_device_loader(
+                    self.args.device
+                )
+                epoch_iterator = parallel_loader
+            else:
+                epoch_iterator = train_dataloader
+            
+            # Reset the past mems state at the beginning of each epoch if
+            # necessary.
+            if self.args.past_index >= 0:
+                self._past = None
+            epoch_pbar = tqdm(
+                epoch_iterator,
+                desc="Iteration",
+                disable=disable_tqdm)
+            for step, inputs in enumerate(epoch_iterator):
+                # Skip past any already trained steps if resuming training
+                if steps_trained_in_current_epoch > 0:
+                    steps_trained_in_current_epoch -= 1
+                    epoch_pbar.update(1)
+                    continue
+                
+                tr_loss += self.training_step(model, inputs)
+                self.total_flos += self.floating_point_ops(inputs)
+                    
+                if (step + 1) % self.args.gradient_accumulation_steps == 0 or (
+                    # last step in epoch but step is always smaller than
+                    # gradient_accumulation_steps
+                    len(epoch_iterator) <= self.args.gradient_accumulation_steps
+                    and (step + 1) == len(epoch_iterator)
+                ):
+                    if self.args.fp16 and _use_native_amp:
+                        self.scaler.unscale_(self.optimizer)
+                        torch.nn.utils.clip_grad_norm_(
+                            model.parameters(), self.args.max_grad_norm)
+                    elif self.args.fp16 and _use_apex:
+                        torch.nn.utils.clip_grad_norm_(amp.master_params(
+                            self.optimizer), self.args.max_grad_norm)
+                    else:
+                        torch.nn.utils.clip_grad_norm_(
+                            model.parameters(), self.args.max_grad_norm)
+
+                    if is_torch_tpu_available():
+                        xm.optimizer_step(self.optimizer)
+                    elif self.args.fp16 and _use_native_amp:
+                        self.scaler.step(self.optimizer)
+                        self.scaler.update()
+                    else:
+                        self.optimizer.step()
+
+                    self.lr_scheduler.step()
+                    model.zero_grad()
+                    self.global_step += 1
+                    self.epoch = epoch + (step + 1) / len(epoch_iterator)
+
+                    if (self.args.logging_steps > 0 and self.global_step % self.args.logging_steps == 0) or (
+                        self.global_step == 1 and self.args.logging_first_step
+                    ):
+                        logs: Dict[str, float] = {}
+                        tr_loss_scalar = tr_loss.item()
+                        logs["loss"] = (
+                            tr_loss_scalar - logging_loss_scalar) / self.args.logging_steps
+                        # backward compatibility for pytorch schedulers
+                        logs["learning_rate"] = (
+                            self.lr_scheduler.get_last_lr()[0]
+                            if version.parse(torch.__version__) >= version.parse("1.4")
+                            else self.lr_scheduler.get_lr()[0]
+                        )
+                        logs["global_step"] = self.global_step
+                        logs["epoch"] = int(
+                            epoch) + np.round(step / len(train_dataloader), 3)
+                        logging_loss_scalar = tr_loss_scalar
+
+                        self.log(logs)
+
+                    if (
+                        self.global_step % self.args.eval_steps == 0
+                    ):
+                        metrics = self.evaluate()
+
+                        self._report_to_hp_search(trial, epoch, metrics)
+                        validation_stat['accuracy'][epoch] = metrics['eval_accuracy']
+                        validation_stat['loss'][epoch] = metrics['eval_loss']
+
+                    if self.args.save_steps > 0 and self.global_step % self.args.save_steps == 0:
+                        if validation_stat['accuracy'][:epoch + 1].max(
+                        ) == validation_stat['accuracy'][epoch]:
+                            # In all cases (even distributed/parallel), self.model is always a reference
+                            # to the model we want to save.
+                            if hasattr(model, "module"):
+                                assert (
+                                    model.module is self.model
+                                ), f"Module {model.module} should be a reference to self.model"
+                            else:
+                                assert model is self.model, f"Model {model} should be a reference to self.model"
+                            # Save model checkpoint
+#                             checkpoint_folder = f"{PREFIX_CHECKPOINT_DIR}-{self.global_step}"
+#                             if self.hp_search_backend is not None and trial is not None:
+#                                 run_id = (
+#                                     trial.number
+#                                     if self.hp_search_backend == HPSearchBackend.OPTUNA
+#                                     else tune.get_trial_id()
+#                                 )
+#                                 checkpoint_folder += f"-run-{run_id}"
+                            output_dir = self.args.output_dir
+
+                            self.store_flos()
+                            self.save_model(output_dir)
+
+                            if self.is_world_process_zero():
+                                self._rotate_checkpoints(use_mtime=True)
+
+#                             if is_torch_tpu_available():
+#                                 xm.rendezvous("saving_optimizer_states")
+#                                 xm.save(
+#                                     self.optimizer.state_dict(), os.path.join(
+#                                         output_dir, "optimizer.pt"))
+#                                 with warnings.catch_warnings(record=True) as caught_warnings:
+#                                     xm.save(
+#                                         self.lr_scheduler.state_dict(), os.path.join(
+#                                             output_dir, "scheduler.pt"))
+#                                 reissue_pt_warnings(caught_warnings)
+#                             elif self.is_world_process_zero():
+#                                 torch.save(
+#                                     self.optimizer.state_dict(), os.path.join(
+#                                         output_dir, "optimizer.pt"))
+#                                 with warnings.catch_warnings(record=True) as caught_warnings:
+#                                     torch.save(
+#                                         self.lr_scheduler.state_dict(), os.path.join(
+#                                             output_dir, "scheduler.pt"))
+#                                 reissue_pt_warnings(caught_warnings)
+
+                epoch_pbar.update(1)
+                if self.args.max_steps > 0 and self.global_step >= self.args.max_steps:
+                    break
+            epoch_pbar.close()
+            train_pbar.update(1)
+
+#             if self.args.evaluation_strategy == EvaluationStrategy.EPOCH:
+#                 metrics = self.evaluate()
+#                 self._report_to_hp_search(trial, epoch, metrics)
+
+            if self.args.tpu_metrics_debug or self.args.debug:
+                if is_torch_tpu_available():
+                    # tpu-comment: Logging debug metrics for PyTorch/XLA
+                    # (compile, execute times, ops, etc.)
+                    xm.master_print(met.metrics_report())
+                else:
+                    logger.warning(
+                        "You enabled PyTorch/XLA debug metrics but you don't have a TPU "
+                        "configured. Check your training configuration if this is unexpected."
+                    )
+            if self.args.max_steps > 0 and self.global_step >= self.args.max_steps:
+                break
+            if epoch > 1:
+                if validation_stat['loss'][epoch - early_stopping_epochs:epoch+1].argmax() == early_stopping_epochs:
+                    break
+
+        train_pbar.close()
+        if stat_file_for_saving is not None:
+            json.dump({i: j[:epoch+1].tolist() for i, j in validation_stat.items()},
+                      Path(self.args.output_dir, stat_file_for_saving).open('w'))
+
+        if self.args.past_index and hasattr(self, "_past"):
+            # Clean the state at the end of training
+            delattr(self, "_past")
+
+        logger.info(
+            "\n\nTraining completed. Do not forget to share your model on huggingface.co/models =)\n\n")
+        return TrainOutput(self.global_step, tr_loss.item() / self.global_step)
+
---
+++ scripts/utils.py
@@ -0,0 +1,102 @@
+import torch
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset
+from transformers import EvalPrediction
+from datasets import load_dataset
+import random
+import re
+import jsonlines
+from typing import Sequence, Dict, Any, List
+
+
+def clear_texts(list_of_texts: List[str]) -> List[str]:
+    return [re.sub(r"(\[\[)|(\]\])", "", i) for i in list_of_texts]
+
+
+def clean_text(text: str) -> str:
+    text = text.lower()
+    text = re.sub(r"[^\w0-9 ]+", "", text)
+    text = re.sub(r"\s\s+", " ", text).strip()
+    return text
+
+
+def random_seed(seed: int) -> None:
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+# def get_data_from_file(path: str) -> List[Any]:
+#     text_file = open(path, "r")
+#     raw_lines = text_file.readlines()
+#     dataset = [(' '.join(i.split()[1:]), int(i.split()[0])) for i in raw_lines]
+#     return dataset
+
+
+def get_data_from_file(path: str) -> List[Dict[str, Any]]:
+    data = []
+    with jsonlines.open(path, "r") as reader:
+        for items in reader:
+            data.append(items)
+    dataset = [(data['text'], int(data['label'])) for i in data]
+    return dataset
+
+
+def get_adv_data(path: str) -> List[Any]:
+    df = pd.read_csv(path)
+    df = df[df['result_type'] == 'Successful']
+    labels = [int(i) for i in df['ground_truth_output'].tolist()]
+    texts = [re.sub(r"(\[\[)|(\]\])", "", i)
+             for i in df['perturbed_text'].tolist()]
+    return [(t, l) for t, l in list(zip(texts, labels))]
+
+
+def get_data_huggingface(data_args: Any, split: str = 'train') -> List[Any]:
+    if data_args.huggingface_subset_name is None:
+        data = load_dataset(data_args.huggingface_name)
+    else:
+        data = load_dataset(
+            data_args.huggingface_name,
+            data_args.huggingface_subset_name)
+    return data
+
+
+class CustomDataset(Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx])
+                for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+
+def compute_metrics(p: EvalPrediction):
+    is_regression = False
+    preds = p.predictions[0] if isinstance(
+        p.predictions, tuple) else p.predictions
+    preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
+    accuracy = (preds == p.label_ids).astype(np.float32).mean().item()
+    return {"accuracy": accuracy}
+
+
+task_to_keys = {
+    "glue^cola": ("sentence", None),
+    "glue^mnli": ("premise", "hypothesis"),
+    "glue^mrpc": ("sentence1", "sentence2"),
+    "glue^qnli": ("question", "sentence"),
+    "glue^qqp": ("question1", "question2"),
+    "glue^rte": ("sentence1", "sentence2"),
+    "glue^sst2": ("sentence", None),
+    "glue^stsb": ("sentence1", "sentence2"),
+    "glue^wnli": ("sentence1", "sentence2"),
+    "rotten_tomatoes": ("text", None),
+    "ag_news": ("text", None),
+    "dstc": ("text", None)
+}
