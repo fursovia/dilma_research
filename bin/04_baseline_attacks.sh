132282c08eb6c35471051b1e9bfb4b1c6d3a1cda
burnyshev <pavel.burnysheff@yandex.ru>
2021-01-27 15:32:04 +0000

    fix

--- bin/04_baseline_attacks.sh
+++ bin/04_baseline_attacks.sh
@@ -81,7 +81,7 @@ for dataset in "rotten_tomatoes" "ag_news" "sst2" "dstc"; do
             textattack attack \
                 --recipe ${attacker} \
                 --model-from-file ./presets/transformer_substitute_models/${dataset}/load_model.py
-                --dataset-from-file data/${dataset}/load_test.py \
+                --dataset-from-file data/${dataset}/load_substitute_train.py \
                 --num-examples ${NUM_EXAMPLES} \
                 --log-to-csv ${RESULTS_DIR}/roberta_${dataset}_${attacker}.csv \
                 --disable-stdout
