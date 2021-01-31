f27eb5661d7e9df40f6c5475fd43f8f10075f09e
fursovia <fursov.ia@gmail.com>
2021-01-31 16:28:55 +0300

    ad training

--- bin/06_adversarial_detection.sh
+++ bin/06_adversarial_detection.sh
@@ -6,11 +6,19 @@
 # 2. train a LSTM classifier to detect adversarial-vs-non-adversarial
 # 3. calculate ROC AUC, Accuracy metrics. Save table to ./results
 
-for dataset in "rotten_tomatoes" "sst2" "ag_news" "dstc"; do
-  PYTHONPATH=. python scripts/convert_attack_output_for_adv_detection_trainuing.py --dataset-dir ./data/${dataset}
+DATE=$(date +%H%M%S-%d%m)
+CONFIG_PATH=./configs/models/clf_lstm.jsonnet
 
-  TRAIN_DATA_PATH=./data/${dataset}/ad_train.json \
-      VALID_DATA_PATH=./data/${dataset}/ad_valid.json \
+for path in ./data/detection/*; do
+
+  name=$(basename ${path})
+  name="${name%.*}"
+
+  EXP_NAME="detection-${name}"
+  LOG_DIR=./logs/${DATE}/${EXP_NAME}
+
+  TRAIN_DATA_PATH=${path}/train.json \
+      VALID_DATA_PATH=${path}/valid.json \
       allennlp train ${CONFIG_PATH} \
       --serialization-dir ${LOG_DIR} \
       --include-package dilma
