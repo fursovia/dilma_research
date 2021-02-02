dddf5e12b66d1d18e353836b222a2b7aa3255082
fursovia <fursov.ia@gmail.com>
2021-02-02 12:23:04 +0300

    refactor

--- dilma/__init__.py
+++ dilma/__init__.py
@@ -1,4 +1,3 @@
 from dilma.models.classifier import BasicClassifierOneHotSupport
-from dilma.models.pair_classifier import PairClassifier
 from dilma.models.deep_levenshtein import DeepLevenshtein
 from dilma.reader import PairwiseReader, DeepLevenshteinReader
--- dilma/attackers/__init__.py
+++ dilma/attackers/__init__.py
@@ -1,4 +1,3 @@
 from .attacker import Attacker
 from .dilma import DILMA
 from .sampling_fool import SamplingFool
-from .pair_dilma import PairDILMA
